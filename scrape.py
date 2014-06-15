50c89d35ec7df988ab4c9b48864038f351bc5163
Saul <s.shanabrook@gmail.com>
2014-06-15 15:40:31 -0400

    add error handling for cookies and remove stale cookie hard coded

--- scrape.py
+++ scrape.py
@@ -1,5 +1,9 @@
+from __future__ import print_function
+from __future__ import unicode_literals
+
 import requests
 import os
+import sys
 
 s = requests.Session()
 
@@ -17,6 +21,16 @@ get_some_cookies_from_url = "http://www.ocf.dc.gov/serv/download.asp"
 s.post(get_some_cookies_from_url, options)
 
 download_url = 'http://www.ocf.dc.gov/serv/download_conexp.asp'
-r = requests.post(download_url, options, cookies={'ASPSESSIONIDSSSABCTT': 'FDIANGHCMOGMHOPLKPAKMKPF'})
+r = requests.post(download_url, options)
 
-print r.text,
+# raise error if not 200
+r.raise_for_status()
+
+session_expire_response = ('\r\n\t<script language="javascript">\r\n\t\talert'
+                           '("Your Session is expired. Please try again");\r'
+                           '\n\t\topener.location.href="/serv/download.asp";'
+                           '\r\n\t\twindow.close();\r\n\t</script>\r\n')
+if r.text == session_expire_response:
+    print('Failed in retrieving ', file=sys.stderr)
+else:
+    print(r.text, end="")
