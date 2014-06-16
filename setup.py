9c9a7ab20bacfacbe792b372501b92bf3fc54a4e
Saul <s.shanabrook@gmail.com>
2014-06-16 14:28:20 -0400

    Bump version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dc-campaign-finance-data",
-    version="0.2.0",
+    version="0.3.0",
     packages=find_packages(),
     entry_points={
         'console_scripts': [
